0f9051792bce588f03c091bbe2b80c172d4696bd
RickityRick-claw <namanphotos24@gmail.com>
2026-04-27 04:23:03 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,1 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/vijayabhava/'
-})
+import { defineConfig } from 'vite'; export default defineConfig({ base:'/vijayabhava/' });
